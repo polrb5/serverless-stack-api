aaa4809674506c8d6f6ec2da5f34820cb09d3033
Pol Reig <polreigbroto@gmail.com>
2022-02-25 10:06:37 +0100

    testing userid

--- create.js
+++ create.js
@@ -8,7 +8,8 @@ export const main = handler(async (event, context) => {
     TableName: process.env.tableName,
     Item: {
       // The attributes of the item to be created
-      userId: event.requestContext.identity.cognitoIdentityId, // The id of the author
+      // userId: event.requestContext.identity.cognitoIdentityId, // The id of the author
+      userId: "eu-north-1:19ddf2ae-f959-4ac7-b273-44a0d30acef9",
       noteId: uuid.v1(), // A unique uuid
       content: data.content, // Parsed from request body
       attachment: data.attachment, // Parsed from request body
